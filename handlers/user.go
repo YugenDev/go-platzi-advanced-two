be05e4df79dd14fe6041bf8127ed5241eed11e58
yugoso8512 <yugoso8512@gmail.com>
2024-07-09 16:45:25 +0000

    fix: get server by instance and not via pointer

--- handlers/user.go
+++ handlers/user.go
@@ -20,7 +20,7 @@ type SignUpResponse struct {
 	Email string `json:"email"`
 }
 
-func SignUpHandler(s *server.Server) http.HandlerFunc {
+func SignUpHandler(s server.Server) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var request = SignUpRequest{}
 		err := json.NewDecoder(r.Body).Decode(&request)
