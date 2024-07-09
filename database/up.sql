6f96eeb48017486ea66cfbca032834de4be93f47
yugoso8512 <yugoso8512@gmail.com>
2024-07-09 21:41:48 +0000

    Added table posts

--- database/up.sql
+++ database/up.sql
@@ -6,3 +6,13 @@ CREATE TABLE users (
     password TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
+
+DROP TABLE IF EXISTS posts;
+
+CREATE TABLE posts (
+    id VARCHAR(32) PRIMARY KEY,
+    post_content VARCHAR(256) NOT NULL,
+    create_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    user_id VARCHAR(128) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE  
+);
