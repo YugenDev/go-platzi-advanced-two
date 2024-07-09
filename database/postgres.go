12fbb420f16275117bf3326464a584caf4707c9a
yugoso8512 <yugoso8512@gmail.com>
2024-07-09 15:41:08 +0000

    hofix: unreachable returns

--- database/postgres.go
+++ database/postgres.go
@@ -17,37 +17,38 @@ func NewPostgresRepository(url string) (*PostgresRepository, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	return &PostgresRepository{db}, nil
+}
+
+func (repo *PostgresRepository) Close() error {
+	return repo.db.Close()
 }
 
 func (repo *PostgresRepository) InsertUser(ctx context.Context, user *models.User) error {
 	_, err := repo.db.ExecContext(ctx, "INSERT INTO users (email, password) VALUES ($1, $2)", user.Email, user.Password)
-	if err != nil {
-		return err
-	}
-	return nil
+	return err
 }
 
-func (repo *PostgresRepository) GetUserById(ctx context.Context, id int64) (models.User, error) {
-	rows, err := repo.db.QueryContext(ctx, "SELECT id email FROM users WHERE id = $1", id)
-
+func (repo *PostgresRepository) GetUserByID(ctx context.Context, id int64) (*models.User, error) {
+	rows, err := repo.db.QueryContext(ctx, "SELECT id, email FROM users WHERE id = $1", id)
+	if err != nil {
+		return nil, err
+	}
 	defer func() {
 		err = rows.Close()
 		if err != nil {
 			log.Fatal(err)
 		}
 	}()
-
 	var user = models.User{}
 	for rows.Next() {
 		if err = rows.Scan(&user.Id, &user.Email); err == nil {
 			return &user, nil
 		}
 	}
-
 	if err = rows.Err(); err != nil {
 		return nil, err
 	}
-
 	return &user, nil
 }
