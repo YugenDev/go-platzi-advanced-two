16b3eab88f9a61b5020a3ac2b7e017c49ef8af37
yugoso8512 <yugoso8512@gmail.com>
2024-07-09 13:27:39 +0000

    server module

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.21.10
 
 require (
 	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
-	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/gorilla/mux v1.8.1
 	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 )
---
+++ server/server.go
@@ -0,0 +1,61 @@
+package server
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Config struct {
+	Port         string
+	JWTSecretKey string
+	DatabaseURL  string
+}
+
+type Server interface {
+	Config() *Config
+}
+
+type Broker struct {
+	ConfigFile *Config
+	Router     *mux.Router
+}
+
+func (b *Broker) Config() *Config {
+	return b.ConfigFile
+}
+
+func NewServer(ctx context.Context, config *Config) (*Broker, error) {
+	if config.Port == "" {
+		return nil, errors.New("port is required")
+	}
+
+	if config.JWTSecretKey == "" {
+		return nil, errors.New("secret is required")
+	}
+
+	if config.DatabaseURL == "" {
+		return nil, errors.New("database url is required")
+	}
+
+	broker := &Broker{
+		ConfigFile: config,
+		Router:     mux.NewRouter(),
+	}
+
+	return broker, nil
+}
+
+func (b *Broker) Start(binder func(s Server, r *mux.Router)) {
+	b.Router = mux.NewRouter()
+	binder(b, b.Router)
+
+	log.Println("Initializing server on port: ", b.Config().Port)
+	if err := http.ListenAndServe(b.ConfigFile.Port, b.Router); err != nil {
+		log.Println("Failed to start server: ", err)
+	}
+
+}
