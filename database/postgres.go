dbae53dbfd55bc4c10a36745c6f268f68cf67dfb
yugoso8512 <yugoso8512@gmail.com>
2024-07-11 15:27:18 +0000

    Added method get post by id

--- database/postgres.go
+++ database/postgres.go
@@ -81,3 +81,26 @@ func (repo *PostgresRepository) InsertPost(ctx context.Context, post *models.Pos
 	_, err := repo.db.ExecContext(ctx, "INSERT INTO posts (id, post_content, user_id) VALUES ($1, $2, $3)", post.Id, post.PostContent, post.UserId)
 	return err
 }
+
+func (repo *PostgresRepository) GetPostById(ctx context.Context, id string) (*models.Posts, error) {
+	rows, err := repo.db.QueryContext(ctx, "SELECT id, post_content, created_at, user_id FROM posts WHERE id = $1", id)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		err = rows.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+	var post = models.Posts{}
+	for rows.Next() {
+		if err = rows.Scan(&post.Id, &post.PostContent, &post.CreatedAt, &post.UserId); err == nil {
+			return &post, nil
+		}
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+	return &post, nil
+}
